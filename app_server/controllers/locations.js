a46be2861e9cbf119b1b3bb6a918605f0dd11404
swirfneblin <swirfneblin@gmail.com>
2016-08-18 21:32:00 -0300

    controllers and routes

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,12 @@
+
+module.exports.homeList = function(req, res){
+	res.render('index', { title : 'Home'});
+};
+
+module.exports.locationInfo = function(req, res){
+	res.render('index', { title : 'Location info'});
+};
+
+module.exports.addReview = function(req, res){
+	res.render('index', { title : 'Add review'});
+};
