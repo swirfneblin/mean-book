2d0159f77268664710c2724418b2aee9fd965399
swirfneblin <swirfneblin@gmail.com>
2016-08-23 01:33:58 -0300

    adicionando template das views

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -8,5 +8,5 @@ module.exports.locationInfo = function(req, res){
 };
 
 module.exports.addReview = function(req, res){
-	res.render('index', { title : 'Add review'});
+	res.render('location-review-form', { title : 'Add review'});
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,5 @@
 /* GET 'about' page */
 
 module.exports.about = function(req, res){
-	res.render('index', { title : 'About'});
+	res.render('generic-text', { title : 'About'});
 };
