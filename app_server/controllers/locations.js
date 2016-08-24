367fb3eef910d42ff56a01e8b775c1ed656aed95
swirfneblin <swirfneblin@gmail.com>
2016-08-23 23:14:00 -0300

    dados de layout dentro do controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,36 @@
 
 module.exports.homeList = function(req, res){
-	res.render('locations-list', { title : 'Home'});
+	res.render('locations-list', { 
+		title : 'Loc8r - Find places to work with wifi near you',
+		pageHeader : {
+			title : 'Loc8r',
+			strapline : 'Find places to work with wifi near you'   
+		},
+		sidebar: "Loc8r helps places to work with and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+		locations:[
+			{
+				name: 'Starcups',
+				address: '125 High Street, Reading RG6 1PS',
+				rating: 3,
+				facilities: ['Hot Drink', 'Food', 'Premium wifi'],
+				distance: '100m'
+			},
+			{
+				name: 'Cafe hero',
+				address: '125 High Street, Reading RG6 1PS',
+				rating: 4,
+				facilities: ['Hot Drink', 'Food', 'Premium wifi'],
+				distance: '200m'
+			},
+			{
+				name: 'Burguer Queen',
+				address: '125 High Street, Reading RG6 1PS',
+				rating: 2,
+				facilities: ['Food', 'Premium wifi'],
+				distance: '250m'
+			}
+		]
+	});
 };
 
 module.exports.locationInfo = function(req, res){
